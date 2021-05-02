6bf5015d5d5604ade02b48144d0d6131de4d3661
AlexRussovits <sasikrus2002@gmail.com>
2021-05-02 16:37:10 +0300

    app repo

---
+++ app/Country.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    protected $table = 'country';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'Code',
+        'Name',
+        'Continent',
+        'Region',
+        'IndepYear',
+        'Population',
+        'GovernmentForm',
+        'HeadofState',
+        'Code2'
+    ];
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('start');
+});
+Route::get('/countries', 'CountryController@index');
+Route::get('detail/{country}', 'CountryController@show');
+Route::get('/cities', 'CityController@index');
+Route::get('/search/', 'CountryController@search')->name('search');
+Route::get('/continent', 'CountryController@listContinent');
+Route::get('continentCountry/{continent}', 'CountryController@listContinent');
+
